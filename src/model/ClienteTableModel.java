c5ee45016d8e6209f3b226906409476f07f4eb1a
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2025-05-23 00:26:47 -0300

    Criando modelo cliente

---
+++ src/model/ClienteTableModel.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author mrblue
+ */
+public class ClienteTableModel extends AbstractTableModel {
+
+    private List<Cliente> clientes;
+
+    private String[] colunas = {"id", "nome", "sobrenome", "rg", "cpf", "endereço"};
+
+    ClienteTableModel() {
+        this.clientes = new ArrayList<>();
+    }
+
+    ClienteTableModel(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public Cliente getCliente(int rowIndex) {
+        return clientes.get(rowIndex);
+    }
+
+    @Override
+    public int getRowCount() {
+        return clientes.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Cliente cliente = clientes.get(rowIndex);
+
+        switch (columnIndex) {
+            case 0:
+                return cliente.getId();
+            case 1:
+                return cliente.getNome();
+            case 2:
+                return cliente.getSobrenome();
+            case 3:
+                return cliente.getRg();
+            case 4:
+                return cliente.getCpf();
+            case 5:
+                return cliente.getEndereco();
+            default:
+                return null;
+        }
+
+    }
+}
