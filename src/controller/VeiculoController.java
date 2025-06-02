ba8891ef25375e24bd9a9706846b683eca4d4ec8
note <henriquekkrrr@gmail.com>
2025-06-02 20:24:35 -0300

    Adicionada tela para cadastro de veiculo

--- src/controller/VeiculoController.java
+++ src/controller/VeiculoController.java
@@ -1,5 +1,14 @@
 package controller;
 
+import dao.VeiculoDAO;
+import model.Veiculo;
+
 public class VeiculoController {
+
+    private VeiculoDAO dao = new VeiculoDAO();
+
+    public void incluirVeiculo(Veiculo veiculo) {
+        dao.adicionarVeiculo(veiculo);
+    } 
 
 }
